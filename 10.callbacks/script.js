2c52c3c5e634b097b74f0ea1c7c7365049f3cc3a
MYusron12 <ryusron19@gmail.com>
2020-05-23 13:35:55 +0700

    callbacks

---
+++ 10.callbacks/script.js
@@ -0,0 +1,24 @@
+//callback
+//synchronous callback
+        // const mhs = [
+        //     {
+        //         "nama": "Yusron",
+        //         "umur": "23",
+        //         "id": 1
+        //     },
+        //     {
+        //         "nama": "Dinda",
+        //         "umur": "20",
+        //         "id": 2
+        //     }
+        // ];
+// function halo(nama){
+//     alert(`Halo, ${nama}`);
+// }
+// function tampilkanPesan(callback){
+//     const nama = prompt('Masukan Nama : ');
+//     callback(nama);
+// }
+// tampilkanPesan(nama => alert(`Halo, ${nama}`));
+
+//asynchronous callback
